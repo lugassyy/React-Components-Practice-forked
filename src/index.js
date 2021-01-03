7da9ee61a1e8b5f34d630bf2c819fb0ac054ee5f
lugassyy <74782252+lugassyy@users.noreply.github.com>
2021-01-03 18:00:45 +0200

    Now complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Heading from "./Heading";
+import App from "./App";
 
-ReactDOM.render(<Heading />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
